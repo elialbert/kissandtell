508cacaf13de9aff42571250ffe40da3da1f7a5e
elialbert <eli@simplerelevance.com>
2013-08-28 19:31:21 -0500

    updated fb pulling to be better

--- app/js/services.js
+++ app/js/services.js
@@ -5,40 +5,83 @@
 angular.module('3vent.services', ['firebase']).
     service("fbData", function() {
 	console.log("running loaddata service");
+	var dt = new Date();
+	var early = Math.round(new Date().setDate(new Date().getDate() - 1) / 1000);
+	var late = Math.round(new Date().setDate(new Date().getDate() + 21) / 1000);
+	var step = 100;
 	var pullingFB = {};
 	var getEventsLooper = function(idx, eventsDB, refreshDB) {
-	    console.log("setting pullingfb to true");
+	    console.log("setting pullingfb to true, step is " + step);
 	    pullingFB.status=true;
 	    console.log("in getEventsLooper, idx is " + idx);
+	    var data = {};
+	    if (idx == 0) {
+		FB.api(
+		    {
+			method: 'fql.query',
+			query: "SELECT eid, name, description, start_time, location, all_members_count, attending_count, creator, declined_count, end_time, has_profile_pic, host, pic, venue FROM event WHERE start_time >= " + early + " AND start_time <= " + late + " AND eid IN (SELECT eid FROM event_member WHERE uid=me())"
+		    },
+		    function(resp) {
+			var extra_data = {};
+			console.dir(resp);
+			console.log("preparing self event data to update");
+			_.each(resp, function(el) {
+			    // eventsDB.child(el.eid).update(el); //very slow way
+			    el.selfEvent = true;
+			    data[el.eid] = el;
+			});			
+			console.log("self data is ");
+			console.dir(data);
+		    }
+
+
+		)}
+	    else {
+		data = {};
+	    }
+
+
 	    FB.api(
 		{
 		    method: 'fql.query',
+		    
 		    //query: 'SELECT uid2 FROM friend WHERE uid1=me()' // for testing
-		    query: "SELECT eid, name, description, start_time, location, all_members_count, attending_count, creator, declined_count, end_time, has_profile_pic, host, pic, venue FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid IN (SELECT uid2 FROM friend WHERE uid1=me() limit 100 offset " + idx*100 + "))"
+		    query: "SELECT eid, name, description, start_time, location, all_members_count, attending_count, creator, declined_count, end_time, has_profile_pic, host, pic, venue FROM event WHERE start_time >= " + early + " AND start_time <= " + late + " AND eid IN (SELECT eid FROM event_member WHERE uid IN (SELECT uid2 FROM friend WHERE uid1=me() limit " + step + " offset " + idx*step + "))"
 		},
 		function(resp) {
 		    console.log("got the events! " + resp.length);
 		    if (!resp.length) {
-			console.log("all fresh out!");
-			console.dir(resp);
-			refreshDB.update({'latest':new Date()});
-			pullingFB.status = false;
-			return;
+			if (resp.error) {
+			    console.log("error - " + resp.error.message);
+			    step = Math.round(step / 2);
+			    return getEventsLooper(idx, eventsDB, refreshDB);
+			}
+			else {
+			    console.log("all fresh out!");
+			    console.dir(resp);
+			    refreshDB.update({'latest':new Date()});
+			    pullingFB.status = false;
+			    return;
+			}
 		    }
-		    if (idx == 10) {
+		    if (idx == 20) {
 			console.log("breaking prematurely");
 			pullingFB.status = false;
 			return;
 		    }
-		    var data = {};
 		    console.log("preparing event data to update");
 		    _.each(resp, function(el) {
 			// eventsDB.child(el.eid).update(el); //very slow way
+			el.selfEvent = false;
 			data[el.eid] = el;
 		    });
 		    console.log("running big update");
 		    eventsDB.update(data);
 		    console.log("done with big update");
+		    if (step < 100) {
+			step += 10;
+		    }
+		    
 		    return getEventsLooper(idx+1, eventsDB, refreshDB);
 		    
 
